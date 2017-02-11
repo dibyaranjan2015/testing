831739d42fc80968d5ab9022a6675ebdb15187ea
Dibyaranjan <dibyaranjan.avanti@gmail.com>
2017-02-11 21:01:53 +0530

    pagination blog

--- laravel/resources/views/blog/blog.blade.php
+++ laravel/resources/views/blog/blog.blade.php
@@ -1,34 +1,5 @@
 @extends('master')
-@section('bodyclass')
-onload="update_data(); setInterval(update_data,2000);"
-@stop
-@section('script')
-		<script>
 
-		  	//FACEBOOK UP
-		  	//AJAX DOWN
-
-
-			function update_data(){
-				$.ajax({
-	                url:'/blogtemp',
-	                
-	                type:'GET',
-	                success: function(response) {
-	                    console.log(response);
-	                    document.getElementById('blog').innerHTML = response;
-	                },
-	                error: function(response) {
-	                    
-
-	                }
-	            });
-			}
-		</script>
-@stop
-		
-
-		
 		@section('body')
 
 			<!-- breadcrumb start -->
@@ -64,11 +35,40 @@ onload="update_data(); setInterval(update_data,2000);"
 							<!-- ================ -->
 							<div class="masonry-grid row" id='blog'>
 								<!-- masonry grid item start -->
-								Blog will goes here
+								@foreach($blogs as $blog)
+										<div class="masonry-grid-item col-sm-6">
+											<!-- blogpost start -->
+											<article class="blogpost shadow light-gray-bg bordered">
+												
+												<header>
+													<h2><a href="/blog/{{$blog->id}}">{{$blog->subject}}</a></h2>
+													<div class="post-info">
+														<span class="post-date">
+															<i class="icon-calendar"></i>
+															<!-- <span class="day">12</span> -->
+															<span class="month">{{$blog->created_at}}</span>
+														</span>
+														<span class="submitted"><i class="icon-user-1"></i> by <a href="#">{{$blog->name}}</a></span>
+														<span class="comments"><i class="icon-chat"></i> <a href="#">22 comments</a></span>
+													</div>
+												</header>
+												<div class="blogpost-content">
+													<p>{{$blog->message}}</p>
+												</div>
+												<footer class="clearfix">
+													<div class="tags pull-left"><i class="icon-tags"></i> <a href="#">tag 1</a>, <a href="#">tag 2</a>, <a href="#">long tag 3</a></div>
+													<div class="link pull-right"><i class="icon-link"></i><a href="/blog/{{$blog->id}}">Read More</a></div>
+												</footer>
+											</article>
+											<!-- blogpost end -->								
+										</div>
+																
+								@endforeach
 								
+
 							</div>
 							<!-- masonry grid end -->
-
+								{{ $blogs->links() }}
 						</div>
 						<!-- main end -->
 
