f33d731ca864845d2bd76e64f12c30ac8ca67156
Siba Sundar Sahoo <Siba Sundar Sahoo>
2017-02-21 15:17:23 +0530

    reset password edited

--- laravel/resources/views/mail/index.blade.php
+++ laravel/resources/views/mail/index.blade.php
@@ -1,7 +1,30 @@
-<?php if(isset($message)){echo $message;}?>
+@extends('login_master')
+@section('body')
+		<div style="width: 26%;margin-left: 37%;margin-right: 37%;padding-top: 50px;">
+			<h1><span class="logo-font"><span class="text-default">Care Medico</span></span></h1><br />
+			<div style="color: red; font-size: 1.2em; text-align: center;"> 
+				<?php if(isset($message)){echo $message;}?>
+			</div>
+			<div class="widget-container" style="border-radius: 5px;">
+				<div class="form_div" id="reset_form" style="padding: 0px 20px;">
+				<div class="heading">
+					<h3 style="color: rgb(5,150,250);margin: 5px;"><b>Reset Password</b></h3><hr />
+					<span style="color: red;"></span>
+					<form action="/login/forgot/send" method="post">
+					{{csrf_field()}}
+						<div class="form-group has-feedback">
+							<input type="email" name="resetmail" class="form-control" placeholder="Email" required></input>
+							<i class="fa fa-user form-control-feedback"></i>
+						</div>
+						<button type="submit" name="submit0" class="btn btn-primary form-control">Send reset link</button>
+					</form>
+					<!--a href="#" class="logbtn_toggle" id="patient">Sign In as Patient</a-->
+				</div>
+			</div>
+		</div>
+		</div>
+		<script type="text/javascript" src="{{asset('assets/plugins/jquery.min.js')}}"></script>
+		<script type="text/javascript" src="{{asset('assets/bootstrap/js/bootstrap.min.js')}}"></script>
+@stop
 
-<form method='post' action='/login/forgot/send'>
-{{csrf_field()}}
-	<input type='mail' name='resetmail'>
-	<input type='submit' value='Send reset link'>
-</form>
+
--- laravel/resources/views/mail/newpass.blade.php
+++ laravel/resources/views/mail/newpass.blade.php
@@ -1,11 +1,34 @@
-<?php if(Session::has('error')){echo Session::get('error');}?>
-
-
-<form method='post' action='/resetpassword/store'>
-{{csrf_field()}}
-   <input type='hidden' name='fa' value='<?php if(isset($id)){echo $id;} ?>'>	
-   New Password : <input type='password' name='new_pass'>
-   Confirm Password : <input type='password' name='conf_new_pass'>
-   <input type='hidden' name='rl' value='<?php if(isset($link)){echo $link;} ?>'>
-   <input type='submit' value='Reset'>
-</form>
+@extends('login_master')
+@section('body')
+		<div style="width: 26%;margin-left: 37%;margin-right: 37%;padding-top: 50px;">
+			<h1><span class="logo-font"><span class="text-default">Care Medico</span></span></h1><br />
+			<div style="color: red; font-size: 1.2em; text-align: center;"> 
+				<?php if(Session::has('error')){echo Session::get('error');}?>
+			</div>
+			<div class="widget-container" style="border-radius: 5px;">
+				<div class="form_div" id="reset_form" style="padding: 0px 20px;">
+					<div class="heading">
+						<h3 style="color: rgb(5,150,250);margin: 5px;"><b>New Password</b></h3><hr />
+						<span style="color: red;"></span>
+						<form action="/resetpassword/store" method="post">
+						{{csrf_field()}}
+							<input type='hidden' name='fa' value='<?php if(isset($id)){echo $id;} ?>'>
+							<div class="form-group has-feedback">
+								<input type="password" id="password" name="new_pass" class="form-control" placeholder="Password(minimum 6 characters)" required></input>
+								<i class="fa fa-lock form-control-feedback"></i>
+							</div>
+							<div class="form-group has-feedback">
+								<input type="password" id="password1" name="conf_new_pass" class="form-control" placeholder="Retype Password" required></input>
+								<i class="fa fa-lock form-control-feedback"></i>
+							</div>
+							<input type='hidden' name='rl' value='<?php if(isset($link)){echo $link;} ?>'>
+							<button type="submit" name="submit0" class="btn btn-primary form-control">Reset Password</button>
+						</form>
+					<!--a href="#" class="logbtn_toggle" id="patient">Sign In as Patient</a-->
+					</div>
+				</div>
+			</div>
+		</div>
+		<script type="text/javascript" src="{{asset('assets/plugins/jquery.min.js')}}"></script>
+		<script type="text/javascript" src="{{asset('assets/bootstrap/js/bootstrap.min.js')}}"></script>
+@stop
